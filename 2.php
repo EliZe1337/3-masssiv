390ddf4c26284e5a12046df840169f88ff532ea0
EliZe1337 <70193717+EliZe1337@users.noreply.github.com>
2025-12-06 00:16:06 +0300

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,19 @@
+<?php
+$numbers = [1, 2, 9, 8, 0, 4, 2, 3, 7, 5];
+rsort($numbers);
+$count = 0;
+$previous = null;
+$third_max;
+foreach ($numbers as $number) {
+  if ($number !== $previous) {
+    $count++;
+    if ($count === 3) {
+      $third_max = $number;
+      break; 
+    }
+    $previous = $number;
+  }
+}
+  echo "Третье максимальное число: " . $third_max . PHP_EOL;
+
+?>
